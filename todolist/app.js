2b8c1319b239e906fde17d34f04d9f8de0a984bc
Scott-Imandt <scottimandt@gmail.com>
2021-03-27 01:52:32 -0400

    Added DB functionality with a local source, to do list can now add and remove items

--- todolist/app.js
+++ todolist/app.js
@@ -3,11 +3,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js")
+const mongoose = require("mongoose");
 
 const app = express();
-
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
 
 app.set('view engine', 'ejs');
 
@@ -15,25 +13,76 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
 
+mongoose.connect("mongodb://localhost:27017/todolistDB",{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
+
+const itemsSchema = {
+  name: String
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const Item1 = new Item({
+  name: "Welcome to your todolist"
+});
+
+const Item2 = new Item({
+  name: "Hit the + buttom to add a item"
+});
+
+const Item3 = new Item({
+  name: "<- hit this to delete an item"
+});
+
+const defaultItems = [Item1, Item2, Item3];
+
 app.get("/", function(req, res) {
 
-  const day = date.getDate();
-
-  res.render("list", {listTitle: day, newListItems:items});
+  Item.find({}, function(err, foundItems){
+    if (foundItems.length === 0){
+      Item.insertMany(defaultItems, function(err){
+        if(err){
+          console.log(err);
+        }else{
+          console.log("Successfully added default items to the database");
+        }
+      });
+      res.redirect("/");
+    } else{
+      //const day = date.getDate();
+      res.render("list", {listTitle: "Today", newListItems:foundItems});
+    }
+  });
 });
 
 app.post("/", function(req,res){
 
-  const item = req.body.newItem;
+  const itemName = req.body.newItem;
 
-  if(req.body.list === "Work"){
-    workItems.push(item);
-    res.redirect("/work");
-  }
-  else {
-    items.push(item);
-    res.redirect("/");
-  }
+  const item = new Item({
+    name: itemName
+  });
+
+  item.save();
+
+  res.redirect("/");
+
+});
+
+
+app.post("/delete", function(req, res){
+
+  const checkedItemId = req.body.checkbox;
+
+  Item.findByIdAndRemove(checkedItemId, function(err){
+    if(err){
+      console.log(err);
+    }
+    else{
+      console.log(checkedItemId + " Was Removed from the DB");
+    }
+  });
+
+  res.redirect("/")
 });
 
 
